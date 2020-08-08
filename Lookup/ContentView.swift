8d2b8c075ab079cdfd887570896343d9aa99dedc
Jackie Barker <jack.barker@gmail.com>
2020-08-07 22:28:18 -0400

    Initial Commit

---
+++ Lookup/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Lookup
+//
+//  Created by Jackie Barker on 8/7/20.
+//  Copyright © 2020 Jackie Barker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LookupTests/LookupTests.swift
@@ -0,0 +1,34 @@
+//
+//  LookupTests.swift
+//  LookupTests
+//
+//  Created by Jackie Barker on 8/7/20.
+//  Copyright © 2020 Jackie Barker. All rights reserved.
+//
+
+import XCTest
+@testable import Lookup
+
+class LookupTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
